05ac4ebb6e85b7f110e4f662943f918a2290e689
Pedro Santos <pedrohenriqueph004@gmail.com>
2024-04-26 13:03:18 -0300

    Terminando as funções

--- Listas/Agenda/Agenda.c
+++ Listas/Agenda/Agenda.c
@@ -1,88 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 typedef struct Pessoa {
     char nome[45];
     char telefone[21];
+    char email[100];
+    char tipo_telefone[20]; // celular, fixo, residência ou trabalho
     int dia;
     int mes;
     int ano;
 } AGENDA;
 
 static AGENDA contato[5];
+static int qtd_contatos = 0; // Quantidade atual de contatos
 
 void salvaContato();
+void listaContatos();
+void ordenaPorNome();
+bool verificaNomeDuplicado(char *nome);
 
 int main() {
-
     int ope;
 
     do {
-        printf("\t Agenda pessoal \n \t 1 - Salvar Contato \n \t 2 - Alterar Contato \n \t 3 - Buscar Contato \n \t 4 - Apagar Contato \n \t 5 - Listar Contato \n \t 0 - Sair \n");
+        printf("\t Agenda pessoal \n");
+        printf("\t Tamanho da agenda: %d \n", qtd_contatos);
+        printf("\t 1 - Salvar Contato \n");
+        printf("\t 2 - Alterar Contato \n");
+        printf("\t 3 - Buscar Contato \n");
+        printf("\t 4 - Apagar Contato \n");
+        printf("\t 5 - Listar Contato \n");
+        printf("\t 6 - Ordenar Contatos por Nome \n");
+        printf("\t 0 - Sair \n");
         scanf("%d", &ope);
 
-        switch (ope){
-            case 1:  
+        switch (ope) {
+            case 1:
                 salvaContato();
                 break;
-
-            case 2:  
+            case 2: 
+                alterarContato();
                 break;
-
-            case 3:  
+            case 3: 
+                buscarContato();
                 break;
-
-            case 4:  
+            case 4: 
+                apagarContato();
                 break;
-
-            case 5:  
-
-                for(int i = 0; i < 5; i++){
-                    printf("\nO contato de %s possui %s e nasceu em %d/%d/%d\n", 
-                    contato[i].nome,
-                    contato[i].telefone,
-                    contato[i].dia,
-                    contato[i].mes,
-                    contato[i].ano
-                );
-                }
-
+            case 5:
+                listaContatos();
                 break;
-
-            case 0:  
+            case 6:
+                ordenaPorNome();
                 break;
-
-            default: 
-                printf("Opção inválida!");
+            case 0:
+                break;
+            default:
+                printf("Opção inválida!\n");
         }
-
-    } while (ope >=1 && ope <= 5);
+    } while (ope != 0);
 
     return 0;
 }
 
-
-void salvaContato()
-{
-    char nome[45];
-    char telefone[21];
-    int dia, mes, ano;
-    static int linha = 0;
-    
-
-    printf("Quais os dados do contato: \n");
-    printf("Digite o nome: \n");
-    scanf("%s", nome);
-    printf("Digite o telefone: \n");
-    scanf("%s", telefone);
-    printf("Digite a data de nascimento: \n");
-    scanf("%d/%d/%d", &dia, &mes, &ano);
-
-    strcpy(contato[linha].nome, nome);
-    strcpy(contato[linha].telefone, telefone);
-    contato[linha].dia = dia;
-    contato[linha].mes = mes;
-    contato[linha].ano = ano;
-    printf("O contato de %s foi salvo com sucesso\n", nome);
-    linha++;
-}
---
+++ Listas/Agenda/Funcoes.c
@@ -0,0 +1,169 @@
+
+void salvaContato() {
+    if (qtd_contatos >= 5) {
+        printf("Agenda cheia!\n");
+        return;
+    }
+
+    AGENDA novo_contato;
+    printf("Quais os dados do contato: \n");
+    printf("Digite o nome: \n");
+    scanf("%s", novo_contato.nome);
+    if (verificaNomeDuplicado(novo_contato.nome)) {
+        printf("Erro: Já existe um contato com esse nome!\n");
+        return;
+    }
+    printf("Digite o tipo de telefone (celular, fixo, residência, trabalho): \n");
+    scanf("%s", novo_contato.tipo_telefone);
+    printf("Digite o telefone: \n");
+    scanf("%s", novo_contato.telefone);
+    printf("Digite o email: \n");
+    scanf("%s", novo_contato.email);
+    printf("Digite a data de nascimento (dd mm aaaa): \n");
+    scanf("%d %d %d", &novo_contato.dia, &novo_contato.mes, &novo_contato.ano);
+
+    contato[qtd_contatos] = novo_contato;
+    qtd_contatos++;
+    printf("O contato de %s foi salvo com sucesso\n", novo_contato.nome);
+}
+
+void alterarContato() {
+
+    int posicao;
+    char opcao[2];
+    char novo_valor[100];
+
+    printf("Digite a posição do contato que deseja alterar: \n");
+    scanf("%d", &posicao);
+
+    if (posicao < 0 || posicao >= qtd_contatos) {
+        printf("Posição inválida!\n");
+        return;
+    }
+
+    printf("O que você gostaria de alterar?\n");
+    printf("Nome (n), Telefone (t), Email (e), Tipo de Telefone (tp), Data de Nascimento (dn)\n");
+    printf("Digite a opção desejada: \n");
+    scanf("%s", opcao);
+
+    if (strcmp(opcao, "n") == 0) {
+        printf("Digite o novo nome: \n");
+        scanf("%s", novo_valor);
+        if (verificaNomeDuplicado(novo_valor)) {
+            printf("Erro: Já existe um contato com esse nome!\n");
+            return;
+        }
+        strcpy(contato[posicao].nome, novo_valor);
+    } else if (strcmp(opcao, "t") == 0) {
+        printf("Digite o novo telefone: \n");
+        scanf("%s", novo_valor);
+        strcpy(contato[posicao].telefone, novo_valor);
+    } else if (strcmp(opcao, "e") == 0) {
+        printf("Digite o novo email: \n");
+        scanf("%s", novo_valor);
+        strcpy(contato[posicao].email, novo_valor);
+    } else if (strcmp(opcao, "tp") == 0) {
+        printf("Digite o novo tipo de telefone (celular, fixo, residência, trabalho): \n");
+        scanf("%s", novo_valor);
+        strcpy(contato[posicao].tipo_telefone, novo_valor);
+    } else if (strcmp(opcao, "dn") == 0) {
+        printf("Digite a nova data de nascimento (dd mm aaaa): \n");
+        scanf("%d %d %d", &contato[posicao].dia, &contato[posicao].mes, &contato[posicao].ano);
+    } else {
+        printf("Opção inválida!\n");
+    }
+
+    printf("Contato alterado com sucesso!\n");
+}
+
+void buscarContato() {
+    char nome_busca[45];
+    bool encontrado = false;
+
+    printf("Digite o nome do contato que deseja buscar: \n");
+    scanf("%s", nome_busca);
+
+    for (int i = 0; i < qtd_contatos; i++) {
+        if (strcmp(contato[i].nome, nome_busca) == 0) {
+            printf("Contato encontrado:\n");
+            printf("Nome: %s\n", contato[i].nome);
+            printf("Telefone: %s\n", contato[i].telefone);
+            printf("Email: %s\n", contato[i].email);
+            printf("Tipo de Telefone: %s\n", contato[i].tipo_telefone);
+            printf("Data de Nascimento: %d/%d/%d\n", contato[i].dia, contato[i].mes, contato[i].ano);
+            encontrado = true;
+            break;
+        }
+    }
+
+    if (!encontrado) {
+        printf("Contato não encontrado.\n");
+    }
+}
+
+void apagarContato() {
+    int posicao;
+    char confirmacao[2];
+
+    printf("Digite a posição do contato que deseja apagar: \n");
+    scanf("%d", &posicao);
+
+    if (posicao < 0 || posicao >= qtd_contatos) {
+        printf("Posição inválida!\n");
+        return;
+    }
+
+    printf("Tem certeza que deseja apagar o contato %s? (s/n)\n", contato[posicao].nome);
+    scanf("%s", confirmacao);
+
+    if (strcmp(confirmacao, "s") == 0) {
+
+        for (int i = posicao; i < qtd_contatos - 1; i++) {
+            contato[i] = contato[i + 1];
+        }
+        qtd_contatos--; 
+        printf("Contato apagado com sucesso!\n");
+    } else {
+        printf("Operação cancelada.\n");
+    }
+}
+
+
+void listaContatos() {
+    for (int i = 0; i < qtd_contatos; i++) {
+        printf("\n[%d] O contato de %s possui %s (%s), email %s e nasceu em %d/%d/%d\n",
+               i,
+               contato[i].nome,
+               contato[i].telefone,
+               contato[i].tipo_telefone,
+               contato[i].email,
+               contato[i].dia,
+               contato[i].mes,
+               contato[i].ano);
+    }
+}
+
+void ordenaPorNome() {
+    int i, j;
+    AGENDA temp;
+
+    for (i = 0; i < qtd_contatos - 1; i++) {
+        for (j = 0; j < qtd_contatos - i - 1; j++) {
+            if (strcmp(contato[j].nome, contato[j + 1].nome) > 0) {
+                temp = contato[j];
+                contato[j] = contato[j + 1];
+                contato[j + 1] = temp;
+            }
+        }
+    }
+    printf("Contatos ordenados por nome.\n");
+}
+
+bool verificaNomeDuplicado(char *nome) {
+    for (int i = 0; i < qtd_contatos; i++) {
+        if (strcmp(contato[i].nome, nome) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
